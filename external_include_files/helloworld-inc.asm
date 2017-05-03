b33d750f8acdbb09bb913aade8e6a96b07f19458
TJRobson <thomas.j.robson@hotmail.com>
2017-05-03 23:24:34 +1200

    0h denotes a null byte, null byte tells assembly where a string ends in memory

--- external_include_files/helloworld-inc.asm
+++ external_include_files/helloworld-inc.asm
@@ -6,8 +6,10 @@
 %include        'functions.asm'                             ; include our external file
 
 section .data
-msg1    db      'Hello, brave new world!', 0Ah              ; our first message string
-msg2    db      'This is how we recycle in NASM.', 0Ah      ; our second message string
+;msg1    db      'Hello, brave new world!', 0Ah              ; our first message string
+;msg2    db      'This is how we recycle in NASM.', 0Ah      ; our second message string
+msg1    db      'Hello, brave new world!', 0Ah, 0h          ; NOTE the null terminating byte
+msg2    db      'This is how we recycle in NASM.', 0Ah, 0h  ; NOTE the null terminating byte
 
 section .text
 global _start
